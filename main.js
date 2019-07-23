3b1043514883d1d2b378ccc71a43ba7ad2f3903d
Beth Ashton <Mbeth.ashton@gmail.com>
2019-07-22 19:31:10 -0700

    Random movement

--- main.js
+++ main.js
@@ -2,28 +2,46 @@ const mapSize = 4;
 
 var map = [];
 
+var peeps = [
+  {x: 0, y: 3, symbol:"M"},
+  {x: 1, y: 3, symbol:"F"}
+]
+
+
+var day = 0;
+
 function initiateMap(){
   var r = 0;
+  var newMap = [];
   
   while (r < mapSize){
     var row = [];
 
     var c = 0;
     while (c < mapSize){
-      row.push(c)
+      row.push("__")
       c++;
     }
 
-    map.push(row)
+    newMap.push(row)
     r++;
   }
+  
+  peeps.forEach(peep => {
+    newMap[peep.y][peep.x] = peep.symbol;
+  });
+
+  map = newMap;
+
+  return map
 }
 
 function printTable(tbody, map){
+  initiateMap();
+  //find peeps current locations, put in map
+
   var newTbody = document.createElement("tbody");
   var r = 0;
-  
-  console.log(r, mapSize)
   while (r < mapSize){
     var row = document.createElement("tr");
 
@@ -51,5 +69,53 @@ function printTable(tbody, map){
   tbody.parentNode.replaceChild(newTbody, tbody);
   return tbody;
 }
+
+function canMove(subject, movement){
+  var result = false;
+
+  if(subject.x + movement.x >= 0 && subject.x + movement.x < mapSize){
+    if(subject.y + movement.y >= 0 && subject.y + movement.y < mapSize){
+      result = true;
+    }
+  }
+
+  return result;
+}
+
+function move(peep){
+  const options = [
+    {x: 0, y: -1},
+    {x: 0, y: 1},
+    {x: -1, y: 0},
+    {x: 1, y: 0}
+  ];
+  var random = Math.floor(Math.random() * options.length);
+  console.log(random);
+  var movement = options[random];
+  console.log(movement)
+  if (canMove(peep, movement)){
+    peep.x += movement.x;
+    peep.y += movement.y;
+  }
+}
+
+function newDay(n){
+  while (n>0){
+    day ++;
+    
+    peeps.forEach(peep => {
+      move(peep);
+    });
+
+    printTable (document.getElementById("map"), map);
+
+    document.getElementById("day").innerHTML = "Day: " + day;
+    n--;
+
+  }
+}
+
 initiateMap();
 printTable (document.getElementById("map"), map);
+
+document.getElementById("day").innerHTML = "Day: " + day;
