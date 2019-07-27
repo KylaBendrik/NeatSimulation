19e32a22f64d38191be49db1ac7dc176785f7f6d
Beth Ashton <Mbeth.ashton@gmail.com>
2019-07-26 17:44:24 -0700

    How to pause and resume with spacebar?

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
-var stage = document.getElementById('svs'), // Get the canvas element by Id
+var pause = true;
+
+var click = 0;
+
+function random(limit){
+  return (Math.floor(Math.random() * limit))
+}
+
+var stage = document.getElementById('map'), // Get the canvas element by Id
         ctx = stage.getContext('2d'), // Canvas 2d rendering context
-        x = 10, //intial horizontal position of drawn rectangle
-        y = 10, //intial vertical position of drawn rectangle
+        x = random(490), //intial horizontal position of drawn rectangle
+        y = random(490), //intial vertical position of drawn rectangle
         wid = 20, //width of the drawn rectangle
         hei = 20; //height of the drawn rectangle
 
 //Draw Rectangle function		
 function drawRect(x,y,wid,hei) {
-    ctx.fillStyle = '#666'; // Fill color of rectangle drawn
+    ctx.fillStyle = '#ff0000'; // Fill color of rectangle drawn
     ctx.fillRect(x, y, wid, hei); //This will draw a rectangle of 20x20
 }
 
 drawRect(x,y,wid,hei); //Drawing rectangle on initial load
 
+function plusClick() {
+  click += 1;
+  document.getElementById("click").innerHTML = click
+}
+
 //move rectangle inside the canvas using arrow keys
 window.onkeydown = function(event) {
     var keyPr = event.keyCode; //Key code of key pressed
+
   
-    if(keyPr === 39 && x<=460){ 
-        x = x+20; //right arrow add 20 from current
+    if (keyPr === 32){
+      var t = undefined;
+      if (pause === true){
+        pause === false
+        clearInterval(t);
+      } else{
+        pause === true;
+        t = setInterval(clickMove, 1000)
+      }
+
     }
-    else if(keyPr === 37 && x>10){
-        x = x-20; //left arrow subtract 20 from current
-    }
-    else if(keyPr === 38 && y>10) {
-        y = y-20; //top arrow subtract 20 from current
-    }
-    else if(keyPr === 40 && y<=460){
-        y = y+20; //bottom arrow add 20 from current
-    }
-		
+
+
+		 console.log(keyPr);
   	/*clearing anything drawn on canvas
      *comment this below do draw path */
     ctx.clearRect(0,0, 500, 500);
@@ -37,3 +52,23 @@ window.onkeydown = function(event) {
   	//Drawing rectangle at new position
     drawRect(x,y,wid,hei);
 };
+
+function move(numClicks){
+  //move rectangle randomly
+  const moves = [[20, 0],[0, 20],[-20, 0],[0, -20]]
+
+  var move = random(moves.length)
+
+  console.log("move is: " + moves[move][0])
+
+  return moves[move]
+}
+
+
+
+function clickMove(){
+  x += move(1)[0];
+  y += move(1)[1];
+
+  plusClick();
+}
